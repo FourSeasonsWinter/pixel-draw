1acb7c79d3c38038dc31b123bc55dd5cf74d1bab
Luiz <louis_worklad24@outlook.com>
2024-11-20 20:05:26 -0300

    Add camera controls.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,16 +1,58 @@
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float panSpeed = 40_000f;
+    public float smoothTime = 0.5f;
+    public float horizontalBorder = 9.5f;
+    public float verticalBorder = 4.5f;
+
+    public float zoomSpeed = 2f;
+    public float minZoom = 5f;
+    public float maxZoom = 15f;
+
+    private Vector3 dragOrigin;
+    private Vector3 velocity = Vector3.zero;
+    private float targetZoom;
+
+    private void Start()
     {
-        
+        targetZoom = Camera.main.orthographicSize;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            Vector3 targetPosition = transform.position + new Vector3(-pos.x * panSpeed * Time.deltaTime, -pos.y * panSpeed * Time.deltaTime, 0);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, -horizontalBorder, horizontalBorder);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, -verticalBorder, verticalBorder);
+
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+            dragOrigin = Input.mousePosition;
+        }
+
+        HandleZoom();
+    }
+
+    private void HandleZoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll != 0)
+        {
+            targetZoom -= scroll * zoomSpeed;
+            targetZoom = Mathf.Clamp(targetZoom, minZoom, maxZoom);
+        }
+
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, targetZoom, Time.deltaTime * zoomSpeed);
     }
 }
