4d609f3e28f2bfc63786314a78fd787f7bf2a59b
Luiz <louis_worklad24@outlook.com>
2024-11-28 21:01:08 -0300

    Add color to palette on button click.

--- Assets/Scripts/Color Scripts/PaletteManager.cs
+++ Assets/Scripts/Color Scripts/PaletteManager.cs
@@ -4,31 +4,39 @@ using UnityEngine.UI;
 
 public class PaletteManager : MonoBehaviour
 {
+    public GameObject colorButtonPrefab;
+    public GameObject addColorButtonObject;
     public GameObject paletteObject;
+    public GameObject colorPicker;
 
     private List<Color> colorPalette = new();
 
     void Start()
     {
-        for (int i = 0; i < paletteObject.transform.childCount; ++i)
+        UpdateColorPaletteCollection();
+    }
+
+    public void AddColorToPalette()
+    {
+        AddColorToPalette(Color.white);
+    }
+
+    public void AddColorToPalette(Color color)
+    {
+        Instantiate(colorButtonPrefab, paletteObject.transform);
+        paletteObject.transform.GetChild(paletteObject.transform.childCount - 1).GetComponent<Image>().color = color;
+
+        addColorButtonObject.transform.SetAsLastSibling();
+
+        UpdateColorPaletteCollection();
+        colorPicker.SetActive(true);
+    }
+
+    private void UpdateColorPaletteCollection()
+    {
+        for (int i = 0; i < paletteObject.transform.childCount - 1; ++i)
         {
             colorPalette.Add(paletteObject.transform.GetChild(i).gameObject.GetComponent<Image>().color);
         }
-
-        Debug.Log($"Colors in palette: {colorPalette.Count}");
-    }
-
-    public bool IsColorInPalette(Color color)
-    {
-        foreach (var c in colorPalette)
-        {
-            if (c == color) return true;
-        }
-
-        return false;
-    }
-
-    public void UpdatePalette()
-    {
     }
 }
